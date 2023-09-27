d3dbff73f77d987559fb72dec333b4c6405ca235
Diogo Correia <me@diogotc.com>
2023-09-27 23:44:27 +0200

    refactor: use npm builder instead of yarn builder

--- nix/pkg-frontend.nix
+++ nix/pkg-frontend.nix
@@ -1,18 +1,23 @@
-{ mkYarnPackage
+{
+buildNpmPackage
 , nodejs
 }:
-mkYarnPackage {
+buildNpmPackage {
   pname = "ist-delegate-election-frontend";
   version = "1.0.0";
+
   src = ../frontend;
+  npmDepsHash = "sha256-1GK4e12K7uPR76t+EUHcK04CGa1WwUkOazks0H9KkbM=";
 
   nativeBuildInputs = [ nodejs ];
 
-  buildPhase = ''
-    runHook preBuild
+  installPhase = ''
+    runHook preInstall
 
-    yarn build
+    mkdir $out
+    npm run build
+    cp -r build/* $out
 
-    runHook postBuild
+    runHook postInstall
   '';
 }
