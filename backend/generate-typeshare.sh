3235f33b9c0cac9a48acdbbac8d0bf4ec69971db
Diogo Correia <me@diogotc.com>
2023-09-30 23:49:27 +0200

    fix(backend): typeshare config file not being read everytime (#97)

--- backend/generate-typeshare.sh
+++ backend/generate-typeshare.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR=$(dirname -- "$0")
-typeshare --lang typescript --output-file $SCRIPT_DIR/../frontend/src/@types/api.ts $SCRIPT_DIR
+typeshare --lang typescript --output-file $SCRIPT_DIR/../frontend/src/@types/api.ts --config-file $SCRIPT_DIR/typeshare.toml $SCRIPT_DIR
 
 pushd $SCRIPT_DIR/../frontend
 npm run format:typeshare
