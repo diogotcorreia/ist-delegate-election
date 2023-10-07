6d5426d4bdbba41275d2c3364270298519cceb8d
Rafael Oliveira <rafdev.x@gmail.com>
2023-10-07 18:04:22 +0200

    feat: show explicit error is typeshare-cli is not available
    Uses `command -v` for POSIX-compliant syntax.

--- backend/generate-typeshare.sh
+++ backend/generate-typeshare.sh
@@ -2,7 +2,12 @@
 
 SCRIPT_DIR=$(dirname -- "$0")
 
-# `typeshare` must be available; use `cargo install typeshare-cli` or equivalent
+if ! command -v typeshare &>/dev/null; then
+	echo 'FATAL ERROR: A "typeshare" binary must be available.'
+	echo 'Use `cargo install typeshare-cli` or equivalent, and then re-run this script.'
+	exit 1
+fi
+
 typeshare --lang typescript --output-file $SCRIPT_DIR/../frontend/src/@types/api.ts --config-file $SCRIPT_DIR/typeshare.toml $SCRIPT_DIR
 
 pushd $SCRIPT_DIR/../frontend
