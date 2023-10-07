8e9df5f4093d32642db78e41c90ef2a5d42c2ea1
Diogo Correia <me@diogotc.com>
2023-10-07 16:40:59 +0200

    feat(backend): show candidacy and voting periods on admin election card (#136)

--- frontend/src/components/admin/DegreeTypeElections.tsx
+++ frontend/src/components/admin/DegreeTypeElections.tsx
@@ -3,21 +3,30 @@ import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
+  alpha,
   Box,
   Card,
   CardContent,
   Chip,
   Divider,
   IconButton,
+  PaletteMode,
   Tooltip,
   Typography,
+  useTheme,
 } from '@mui/material';
 import Grid from '@mui/material/Unstable_Grid2';
 import { Fragment, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
-import { ElectionDto } from '../../@types/api';
+import { ElectionDto, ElectionStatusDto } from '../../@types/api';
 import useLocalizedString from '../../hooks/useLocalizedString';
 import { DegreeTypeAggregator } from '../../hooks/useSortAndGroupDegrees';
+
+const DATE_FORMAT = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric',
+};
 
 interface Props {
   aggregator: DegreeTypeAggregator;
@@ -85,8 +94,9 @@ function DegreeTypeElections({ aggregator, unverifiedNominationsCountByElection 
         )}
       </AccordionSummary>
       <AccordionDetails>
-        {degreesWithElections.map((dElections) => (
+        {degreesWithElections.map((dElections, i) => (
           <Fragment key={dElections.degree.acronym}>
+            {i !== 0 && <Divider sx={{ mb: 1 }} />}
             <Typography variant='subtitle1' color='textSecondary' component='p'>
               {dElections.degree.acronym}
             </Typography>
@@ -102,7 +112,6 @@ function DegreeTypeElections({ aggregator, unverifiedNominationsCountByElection 
                 />
               ))}
             </Grid>
-            <Divider />
           </Fragment>
         ))}
         {degreesWithoutElections.length > 0 && (
@@ -132,6 +141,26 @@ function DegreeTypeElections({ aggregator, unverifiedNominationsCountByElection 
   );
 }
 
+const electionStatusChipColor: Record<
+  PaletteMode,
+  Record<ElectionStatusDto, string>
+> = Object.freeze({
+  light: {
+    [ElectionStatusDto.Candidacy]: '#3A7676',
+    [ElectionStatusDto.Ended]: '#612957',
+    [ElectionStatusDto.NotStarted]: '#5C5747',
+    [ElectionStatusDto.Processing]: '#AE4102',
+    [ElectionStatusDto.Voting]: '#548119',
+  },
+  dark: {
+    [ElectionStatusDto.Candidacy]: '#84C2C2',
+    [ElectionStatusDto.Ended]: '#B34BA0',
+    [ElectionStatusDto.NotStarted]: '#C1BDAE',
+    [ElectionStatusDto.Processing]: '#FD8B49',
+    [ElectionStatusDto.Voting]: '#99DA44',
+  },
+});
+
 interface ElectionCardProps {
   election: ElectionDto;
   unverifiedNominations: number;
@@ -139,31 +168,71 @@ interface ElectionCardProps {
 
 function ElectionCard({ election, unverifiedNominations }: ElectionCardProps) {
   const { t } = useTranslation();
+  const theme = useTheme();
+
+  const chipColor = electionStatusChipColor[theme.palette.mode][election.status];
 
   return (
     <Grid xs={12} sm={6} md={4}>
       <Card variant='outlined'>
         <CardContent>
-          <Typography variant='subtitle2' color='textSecondary' component='p'>
-            {t('election.round', {
-              count: election.round,
-              ordinal: true,
+          <Box display='flex' justifyContent='space-between' alignItems='flex-start' gap={1}>
+            <Box>
+              <Typography variant='subtitle2' color='textSecondary' component='p'>
+                {t('election.round', {
+                  count: election.round,
+                  ordinal: true,
+                })}
+              </Typography>
+              <Typography variant='subtitle1' component='p' gutterBottom>
+                {election.curricularYear
+                  ? t('election.curricular-year', {
+                      count: election.curricularYear,
+                      ordinal: true,
+                    })
+                  : t('election.curricular-year-none')}
+              </Typography>
+            </Box>
+            <Box display='flex' flexDirection='column' alignItems='flex-end' gap={1}>
+              <Chip
+                size='small'
+                variant='outlined'
+                sx={{
+                  color: chipColor,
+                  borderColor: alpha(chipColor, 0.7),
+                  backgroundColor: alpha(chipColor, 0.05),
+                }}
+                label={t(`election.status.${election.status}`)}
+              />
+              {unverifiedNominations > 0 && (
+                <Chip
+                  size='small'
+                  color='warning'
+                  label={t('election.unverified-nominations-count', {
+                    count: unverifiedNominations,
+                  })}
+                />
+              )}
+            </Box>
+          </Box>
+          <Typography variant='subtitle1'>{t('election.candidacy-period.title')}</Typography>
+          <Typography variant='body2' gutterBottom>
+            {election.candidacyPeriod
+              ? t('election.candidacy-period.subtitle-short', {
+                  start: new Date(election.candidacyPeriod?.start),
+                  end: new Date(election.candidacyPeriod?.end),
+                  formatParams: { start: DATE_FORMAT, end: DATE_FORMAT },
+                })
+              : t('election.candidacy-period.none')}
+          </Typography>
+          <Typography variant='subtitle1'>{t('election.voting-period.title')}</Typography>
+          <Typography variant='body2' gutterBottom>
+            {t('election.voting-period.subtitle-short', {
+              start: new Date(election.votingPeriod.start),
+              end: new Date(election.votingPeriod.end),
+              formatParams: { start: DATE_FORMAT, end: DATE_FORMAT },
             })}
           </Typography>
-          <Typography variant='subtitle1' component='p' gutterBottom>
-            {election.curricularYear
-              ? t('election.curricular-year', {
-                  count: election.curricularYear,
-                  ordinal: true,
-                })
-              : t('election.curricular-year-none')}
-          </Typography>
-          {unverifiedNominations > 0 && (
-            <Chip
-              color='warning'
-              label={t('election.unverified-nominations-count', { count: unverifiedNominations })}
-            />
-          )}
         </CardContent>
       </Card>
     </Grid>
