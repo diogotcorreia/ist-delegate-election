7f32d2eb973158c7257bdf035af221075298a9c5
Rafael Oliveira <rafdev.x@gmail.com>
2023-10-07 20:28:37 +0200

    chore: prompt installation of `typeshare-cli` if it cannot be found (#134)
    The `generate-typeshare.sh` script requires a `typeshare` binary to be
    available, which should be clear to prospective contributors, especially
    since the binary is provided by a crate with a different name (and one
    which is not listed as a dependency).

--- backend/generate-typeshare.sh
+++ backend/generate-typeshare.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env sh
 
 SCRIPT_DIR=$(dirname -- "$0")
+
+if ! command -v typeshare &>/dev/null; then
+	echo 'FATAL ERROR: A "typeshare" binary must be available.'
+	echo 'Use `cargo install typeshare-cli` or equivalent, and then re-run this script.'
+	exit 1
+fi
+
 typeshare --lang typescript --output-file $SCRIPT_DIR/../frontend/src/@types/api.ts --config-file $SCRIPT_DIR/typeshare.toml $SCRIPT_DIR
 
 pushd $SCRIPT_DIR/../frontend
