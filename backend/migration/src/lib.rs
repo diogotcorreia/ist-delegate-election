ef33951d899dcd16c463024bb92560288ade64b8
Diogo Correia <me@diogotc.com>
2023-10-08 18:37:39 +0200

    feat(backend): add user degree override migration
    Related to #130

--- backend/migration/src/lib.rs
+++ backend/migration/src/lib.rs
@@ -3,6 +3,7 @@ pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_create_table;
 mod m20230917_143144_nomination_log;
 mod m20231001_091623_nomination_valid_null;
+mod m20231008_162240_user_degree_override;
 
 pub struct Migrator;
 
@@ -13,6 +14,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20220101_000001_create_table::Migration),
             Box::new(m20230917_143144_nomination_log::Migration),
             Box::new(m20231001_091623_nomination_valid_null::Migration),
+            Box::new(m20231008_162240_user_degree_override::Migration),
         ]
     }
 }
